905f47a69599eef7e63b0ccebdc295ea2d353a44
EternalHeart <hao131462@qq.com>
2026-07-24 13:41:51 +0800

    chore: 移除 V2 后端、Web 与小程序端代码

--- apps/server/src/lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { env } from '../config/env';
-
-export const prisma = new PrismaClient({
-  log: env.isDev ? ['warn', 'error'] : ['error'],
-});
-
-export type { Prisma } from '@prisma/client';
